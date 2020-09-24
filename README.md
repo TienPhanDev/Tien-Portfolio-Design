dbf8c808f2f69d514d48c6c3cd82fa8d517bc441
tien phan <tienphan.dev@gmail.com>
2020-09-24 02:18:21 -0400

    Dropdown menu added

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-https://codepen.io/codot/pen/BorNdz login popup form
-https://codepen.io/kathykato/pen/dzdgPJ homepage layout# Tien-Portfolio-Design
+# Tien-Portfolio-Design
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import { NavLink } from "react-router-dom";
@@ -6,9 +7,15 @@ const Header = () => {
   return (
     <div className="navbar">
       <div>
-        <a href='https://www.medicine2code.com/'><img src="https://img.icons8.com/nolan/80/wordpress.png"/></a>
-        <a href=''><img src="https://img.icons8.com/clouds/80/000000/linkedin.png" /></a>
-        <a href='https://github.com/tienphandev'><img src="https://img.icons8.com/clouds/80/000000/github.png" /></a>
+        <a href="https://www.medicine2code.com/">
+          <img src="https://img.icons8.com/nolan/80/wordpress.png" />
+        </a>
+        <a href="https://www.linkedin.com/in/tien-phan-dc/">
+          <img src="https://img.icons8.com/clouds/80/000000/linkedin.png" />
+        </a>
+        <a href="https://github.com/tienphandev">
+          <img src="https://img.icons8.com/clouds/80/000000/github.png" />
+        </a>
       </div>
       <div className="nav-list">
         <ul className="nav-menu">
@@ -23,11 +30,6 @@ const Header = () => {
             </NavLink>
           </li>
           <li>
-            <NavLink to="/projects" className="nav-links">
-              Portfolio
-            </NavLink>
-          </li>
-          <li>
             <NavLink to="/contact" className="nav-links">
               Contact
             </NavLink>
@@ -35,7 +37,25 @@ const Header = () => {
         </ul>
       </div>
       <div>
-      <h3><span>Full Stack</span> <span>Software Developer</span></h3>
+        <ul class="menu">
+          <li class="dropdown">
+            Portfolio▾
+            <ul class="features-menu">
+              <li>
+                <a href="#">Harder</a>
+              </li>
+              <li>
+                <a href="#">Better</a>
+              </li>
+              <li>
+                <a href="#">Faster</a>
+              </li>
+              <li>
+                <a href="#">Stronger</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </div>
   );
